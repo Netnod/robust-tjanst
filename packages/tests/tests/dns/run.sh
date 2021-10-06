6891b0468175a2960b3c823fa8105da6a2ff6fd2
Patrik Fältström <paf@netnod.se>
2021-10-07 00:57:01 +0200

    Rewrote in python but did not change file name.

--- packages/tests/tests/dns/run.sh
+++ packages/tests/tests/dns/run.sh
@@ -1,5 +1,42 @@
-#!/bin/sh
+#!/usr/bin/env python3
 
-dig $1
+import sys
+import json
+import dns.resolver
 
-# parse result and emit json
+if(len(sys.argv) != 2):
+  print("Error")
+  sys.exit(1)
+
+response = {}
+response['ipv6'] = {}
+response['dnssec'] = {}
+try:
+  answer = dns.resolver.resolve(sys.argv[1], "AAAA")
+  response['ipv6']['status'] = "OK"
+  response['ipv6']['data'] = []
+  for record in answer:
+    response['ipv6']['data'].append(record.to_text())
+except dns.resolver.NoAnswer:
+  response['ipv6']['status'] = "NOK"
+  response['ipv6']['data'] = ["NO AAAA"]
+except dns.resolver.NXDOMAIN:
+  response['ipv6']['status'] = "NOK"
+  response['ipv6']['data'] = ["NO SUCH DOMAIN"]
+
+try:
+  answer = dns.resolver.resolve(sys.argv[1], "DS")
+  response['dnssec']['status'] = "OK"
+  response['dnssec']['data'] = []
+  for record in answer:
+    response['dnssec']['data'].append(record.to_text())
+except dns.resolver.NoAnswer:
+  response['dnssec']['status'] = "NOK"
+  response['dnssec']['data'] = ["NO DS RECORD"]
+except dns.resolver.NXDOMAIN:
+  response['dnssec']['status'] = "NOK"
+  response['dnssec']['data'] = ["NO SUCH DOMAIN"]
+
+print(json.dumps(response))
+
+
