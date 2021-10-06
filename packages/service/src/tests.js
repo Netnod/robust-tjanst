2287f5fae35efde12dcb22fa89a9711987a2c0be
Emil Ahlbäck <emil.ahlback@iteam.se>
2021-10-06 12:55:44 +0200

    prep

--- packages/service/src/tests.js
+++ packages/service/src/tests.js
@@ -1,25 +1,46 @@
 const { sql } = require('slonik')
 const { getDomainByID } = require('./db/queries/domains')
 const { getTestByID, getTestPartsByTestID } = require('./db/queries/tests')
-const { scheduleTestRun } = require('./workers/schedule')
 
+
+function upsertURL(url) {
+  return sql`
+    INSERT INTO domains (domain_name)
+    VALUES (${url})
+    ON CONFLICT ON CONSTRAINT domains_domain_name_key
+    DO UPDATE SET domain_name=domains.domain_name
+    -- This ensures we have an id to return ^
+    RETURNING id AS domain_id
+  `
+}
+
+function insertNewTest(domain_id) {
+  return sql`
+    INSERT INTO domain_tests (domain_id)
+    VALUES (${domain_id}) 
+    RETURNING id
+  `
+}
 
 async function createTest(ctx) {
-  const {domain_id} = ctx.request.body
-  // TODO: Verify this is accessible to ctx.state.user_id
+  // // TODO: Validate URL
+  const {url} = ctx.request.body
+  // // TODO: "Owned" URLs are perhaps "private"
 
-  const id = await ctx.dbPool.connect(async (connection) => {
-    const {id} = await connection.one(sql`
-      INSERT INTO domain_tests (domain_id)
-      VALUES (${domain_id}) 
-      RETURNING id
-    `)
+  // const id = await ctx.dbPool.connect(async (connection) => {
+  //   const {domain_id} = await connection.one(upsertURL(url));
 
-    return id
-  })
+  //   const {id} = await connection.one(insertNewTest(domain_id))
+  //   return id
+  // })
+  // ctx.redirect(ctx.state.namedPath('test_page', {url, id}))
 
-  ctx.redirect(ctx.state.namedPath('test_page', {id}))
-  await scheduleTestRun(id)
+  // // K8S: Schedule!
+  const domain = {domain_name: url} 
+  const parts = [
+    {part_id: 'dns', test_status: 'FIN'}
+  ]
+  await ctx.render('tests/show', {domain, parts})
 }
 
 async function showTest(ctx) {
@@ -29,8 +50,6 @@ async function showTest(ctx) {
     const test = await connection.one(getTestByID(id))
     const domain = await connection.one(getDomainByID(test.domain_id))
     const parts = await connection.any(getTestPartsByTestID(id))
-
-    console.log(parts)
 
     await ctx.render('tests/show', {test, domain, parts})
   })
