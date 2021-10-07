5d0780cae625fa1a562c9486fceae56112bb97d1
Emil Ahlbäck <e.ahlback@gmail.com>
2021-10-07 16:40:27 +0200

    Minimise css, purge unused css classes, also align workspace name with package name (#11)

--- packages/web/postcss.config.js
+++ packages/web/postcss.config.js
@@ -2,5 +2,6 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    require('cssnano')
   ]
 }
--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,7 +1,9 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [
+    'src/views/**/*.pug'
+  ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     fontFamily: {
