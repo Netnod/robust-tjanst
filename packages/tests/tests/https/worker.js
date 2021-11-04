e700f236a4b1b45b8b76b350fd3018b3dd5ec9a6
Adam Näslund <adam.naslund@iteam.se>
2021-11-04 18:29:27 +0100

    chore: more explicit logging in test

--- packages/tests/tests/https/worker.js
+++ packages/tests/tests/https/worker.js
@@ -2,7 +2,7 @@ const { Worker } = require('bullmq')
 const { startTest, deleteTest } = require('../../lib/kubernetes')
 const queue = require('../../index').testQueues.https
 
-const image = 'mullsork/https-reachable:latest'
+const image = 'netnodse/https-reachable:latest'
 const test_name = 'https-reachable'
 
 module.exports = (connection, resultQueue) => new Worker(queue.name, async (job) => {
@@ -14,14 +14,15 @@ module.exports = (connection, resultQueue) => new Worker(queue.name, async (job)
     const pod = await startTest(image, podName, id, [url])
     await pod.done()
     const logs = await pod.log()
-    job.log(logs)
+    job.log('Test returned: ' + logs)
 
     const response = JSON.parse(logs)
     const output = { test_id, test_name, result: {passed: response.result === 'OK'} }
     console.log(response)
     await resultQueue.add(job.name, output);
   } catch (err) {
-    await job.log(JSON.stringify(err))
+    // TODO: log only relevant parts of err, k8s errors are insanely verbose
+    await job.log('Test pod failed with: ' + JSON.stringify(err))
     throw err;
   } finally {
     deleteTest(podName)
