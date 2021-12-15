c9e338033b8e0c9ff71ea8aa86e0804eaa41c3db
izabellalarsson <izzwii.izabella@gmail.com>
2021-12-15 09:53:29 +0100

    add success bolean to group result

--- packages/tests/groupMessages.js
+++ packages/tests/groupMessages.js
@@ -1,22 +1,27 @@
 module.exports = {
-  'https': (tests) => {
+  https: (tests) => {
     return {
-      title: "HTTPS",
-      description: "Säker åtkomst till tjänsten krypterad anslutning",
-      result: 
-        tests.every(t => t.passed)
-        ? "✔️ Kommunikationen är skyddad"
-        : "❌ All kommunikation är inte skyddad",
-    }
+      title: 'HTTPS',
+      description: 'Säker åtkomst till tjänsten krypterad anslutning',
+      result: {
+        success: tests.every((t) => t.passed),
+        description: tests.every((t) => t.passed)
+          ? '✔️ Kommunikationen är skyddad'
+          : '❌ All kommunikation är inte skyddad',
+      },
+    };
   },
-  'dns': (tests) => {
+  dns: (tests) => {
     return {
-      title: "DNS",
-      description: "Rätt uppsatt DNS för ditt domännamn är viktigt för säkerhet och pålitlighet",
-      result: 
-        tests.every(t => t.passed) 
-        ? "✔️ Allt ser bra ut!" 
-        : "❌ Ett eller flera krav är inte uppnådda."
-    }
-  }
-}
+      title: 'DNS',
+      description:
+        'Rätt uppsatt DNS för ditt domännamn är viktigt för säkerhet och pålitlighet',
+      result: {
+        success: tests.every((t) => t.passed),
+        description: tests.every((t) => t.passed)
+          ? '✔️ Allt ser bra ut!'
+          : '❌ Ett eller flera krav är inte uppnådda.',
+      },
+    };
+  },
+};
