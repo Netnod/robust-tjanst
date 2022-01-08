e143b87933c4dcbcb482d03ec35f255d932a6fb7
Patrik Fältström <paf@netnod.se>
2022-01-08 15:40:10 +0100

    Fixed the JSON syntax

--- packages/tests/tests/zonemaster/doit.sh
+++ packages/tests/tests/zonemaster/doit.sh
@@ -10,17 +10,16 @@ if [ "x${ARGDOMAIN}" = x ]; then
     exit 1
 fi
 
-#zonemaster-cli "${ARGDOMAIN}" --no-ipv6 --json_stream --json_translate > /tmp/foo
-zonemaster-cli "${ARGDOMAIN}" --json_stream --json_translate > /tmp/foo
+zonemaster-cli "${ARGDOMAIN}" --no-ipv6 --json_stream --json_translate | tr -d '\n' | sed 's/}{/}, {/g' > /tmp/foo
 
 ## Output should be
 # '{ passed: true/false, details: { object } }'
 
 A=`grep \"level\":\"ERROR\" /tmp/foo`
 if [ "x$A" = x ]; then
-    echo "'{ passed: true, details: "`cat /tmp/foo`" }'"
+    echo "'{ \"passed\": true, \"details\": [ "`cat /tmp/foo`" ] }'"
 else
-    echo "'{ passed: false, details: "`cat /tmp/foo`" }'"
+    echo "'{ \"passed\": false, \"details\": [ "`cat /tmp/foo`" ] }'"
 fi
 
 
