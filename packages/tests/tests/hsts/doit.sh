8f4effab5563c72c5c5a3eb62b3fbd622460012e
Vilhelm Prytz <vilhelm@prytznet.se>
2022-01-11 09:42:23 +0100

    tests/hsts: rewrite check so that headers with includeSubdomains pass
    fixes #41

--- packages/tests/tests/hsts/doit.sh
+++ packages/tests/tests/hsts/doit.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 HOST=$1
-curl -s --hsts cache.txt https://"$HOST"/ > /dev/null
-A=`grep ^"$HOST" cache.txt`
+HEADER=$(curl -s -D- https://"$HOST"/ | grep -i Strict-Transport-Security)
+# AGE=$(echo "$HEADER" | awk -v FS="(max-age=|;)" '{print $2}')
+
 # TODO: look at max-age in the header and options
-if [ "x$A" = x ]; then
+if [ -z "$HEADER" ]; then
    echo "{ \"passed\": false, \"details\": { \"tested_url\": \"https://$HOST/\" } }"
 else
    echo "{ \"passed\": true, \"details\": { \"tested_url\": \"https://$HOST/\", \"host\": \"$HOST\" } }"
 fi
-
