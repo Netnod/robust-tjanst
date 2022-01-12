9c1d9ce3fe339553c0ad1b8b871dd0ed67c86bd7
izabellalarsson <izzwii.izabella@gmail.com>
2022-01-12 10:53:45 +0100

    chore: use new svgs

--- packages/web/src/sigil.js
+++ packages/web/src/sigil.js
@@ -1,5 +1,6 @@
 const { getLatestResult } = require("./db/queries/domains")
-
+const sigilSuccess = require('./assets/robust-badge--animated.js')
+const sigilFail = require('./assets/robust-badge--fail.js')
 async function getSigil(ctx) {
   const {domain} = ctx.request.params
   if (!domain) throw "Boo"
@@ -9,42 +10,8 @@ async function getSigil(ctx) {
 
   const pool = ctx.dbPool
   await pool.connect(async conn => {
-    const result = await getLatestResult(conn, domain)
-
-    if (!result) {
-      ctx.body = `
-        <svg viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg">
-          <style>
-            .small { font: italic 13px sans-serif; }
-          </style>
-
-          <text x="20" y="45" class="small">No result for ${domain}</text>
-        </svg>
-      `
-      return
-    } 
-
-    const fullScore = result.final_score === 100
-    const score = fullScore ? 'good': 'bad'
-
-    ctx.body = `
-        <svg viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg">
-          <style>
-            .small { font: italic 13px sans-serif; }
-            .heavy { font: bold 30px sans-serif; }
-        
-            /* Note that the color of the text is set with the    *
-            * fill property, the color property is for HTML only */
-            .bad { font: italic 40px serif; fill: red; }
-            .good { font: italic 40px serif; fill: green; }
-          </style>
-        
-          <text x="20" y="35" class="small">My</text>
-          <text x="40" y="35" class="heavy">site</text>
-          <text x="55" y="55" class="small">is</text>
-          <text x="65" y="55" class="${score}">${score === 'good' ? 'Robust!' : 'Bad!'}</text>
-        </svg>
-      `
+    // const result = await getLatestResult(conn, domain)
+    ctx.body = sigilSuccess
   })
 
 }
