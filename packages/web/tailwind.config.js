d18c784a74df7522b75a7c92f1e110710a39dc0f
Izabella Larsson <izzwii.izabella@gmail.com>
2022-01-12 17:44:45 +0100

    chore: hero (#44)
    * feat: menu use different logo&icon depending on theme
    
    * chore: add hero to landing page
    
    * style: name change

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -35,6 +35,10 @@ module.exports = {
         lightGreen: '#2ED320',
         lightRed: '#E22121',
       },
+      backgroundImage: {
+        'rock-wall':
+          'linear-gradient(rgba(0,0,0,.8), rgba(0,0,0,.8)),url(/img/bergvagg.jpg)',
+      },
     },
   },
   variants: {
