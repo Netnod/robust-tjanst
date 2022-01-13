aeda5fe5e13cb1410a572c566ea6a564001814ca
Emil Ahlbäck <e.ahlback@gmail.com>
2022-01-13 13:04:36 +0100

    fix: ZM tests output as intended by PAF (#47)

--- packages/tests/tests/zonemaster/messages.js
+++ packages/tests/tests/zonemaster/messages.js
@@ -2,8 +2,8 @@ module.exports = (result) => {
     const title = result.passed ? "DNS är ok" : "DNS är inte ok"
     // This is markdown, one line per warning/error
     const description = result.details
-      .map(detail => `** ${detail.level} ** ${detail.tag} ${detail.message}`)
-      .join("\n")
+      .map(detail => `**${detail.level}** \`${detail.tag}\` ${detail.message}  \n`)
+      .join("")
 
     return {
       passed: result.passed,
