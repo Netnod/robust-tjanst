7ad6ba2e531f3ba80381c4c19ceb17e20977c425
izabellalarsson <izzwii.izabella@gmail.com>
2022-01-21 11:30:40 +0100

    add small sigil

--- packages/web/src/sigil.js
+++ packages/web/src/sigil.js
@@ -1,8 +1,9 @@
 const { getLatestResult } = require("./db/queries/domains")
 const sigilSuccess = require('./assets/robust-badge--animated.js')
+const sigilSuccessSmall = require('./assets/robust-badge--small--animated.js')
 const sigilFail = require('./assets/robust-badge--fail.js')
 async function getSigil(ctx) {
-  const {domain} = ctx.request.params
+  const {domain, type} = ctx.request.params
   if (!domain) throw "Boo"
 
   ctx.type = 'image/svg+xml'
@@ -12,7 +13,12 @@ async function getSigil(ctx) {
   await pool.connect(async conn => {
     // const result = await getLatestResult(conn, domain)
     // TODO: implement check if testes passed or failed 
-    ctx.body = sigilSuccess
+    if (type === 'badge.svg'){
+      ctx.body = sigilSuccess
+    }
+    else {
+      ctx.body = sigilSuccessSmall
+    }
   })
 
 }
