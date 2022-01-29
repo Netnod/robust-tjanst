23db9e6c57a07f0197cd757a576f6b85efa16539
Karl Lindmark <karl.lindmark@gmail.com>
2022-01-29 10:25:22 +0100

    fix: tests/hsts: include host in failure JSON response (#80)

--- packages/tests/tests/hsts/doit.sh
+++ packages/tests/tests/hsts/doit.sh
@@ -6,7 +6,7 @@ HEADER=$(curl -s -D- https://"$HOST"/ | grep -i Strict-Transport-Security)
 
 # TODO: look at max-age in the header and options
 if [ -z "$HEADER" ]; then
-   echo "{ \"passed\": false, \"details\": { \"tested_url\": \"https://$HOST/\" } }"
+   echo "{ \"passed\": false, \"details\": { \"tested_url\": \"https://$HOST/\", \"host\": \"$HOST\" } }"
 else
    echo "{ \"passed\": true, \"details\": { \"tested_url\": \"https://$HOST/\", \"host\": \"$HOST\" } }"
 fi
